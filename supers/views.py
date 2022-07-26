80ce86ec3ca72cbf601aec7ea69f0c81e5c8b5ca
unknown <colost72@gmail.com>
2022-07-26 00:07:55 -0600

    Created the Funcationality to Delete a Specific Super

--- supers/views.py
+++ supers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 from supers import serializers
@@ -9,26 +10,17 @@ from supers import serializers
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-
-        hero_type = request.query_params.get('super_type')
-        print(hero_type)
-
         supers = Super.objects.all()
-        
-        if hero_type:
-            supers = supers.filter(hero_type=hero_type)
-
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=400)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+   
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
     result = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':
@@ -39,4 +31,6 @@ def super_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
+    elif request.method == 'DELETE':
+        result.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

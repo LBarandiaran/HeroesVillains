900e0d7dc2c214ea23465cf36b2fda55db762b76
unknown <colost72@gmail.com>
2022-07-25 20:41:28 -0600

    Completed Supers App + Registered SuperUser to Insert SuperTypes Values

--- supers/models.py
+++ supers/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from super_types.models import SuperType
+
 # Create your models here.
 
 class Super(models.Model):
@@ -8,4 +10,4 @@ class Super(models.Model):
     primary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
-    super_type = ## FK ##
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)

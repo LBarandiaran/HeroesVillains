b10118518fe270ce3dee16d6b926bacac6ae2ec0
unknown <colost72@gmail.com>
2022-07-25 23:21:41 -0600

    Created New View Function for Super by ID

--- supers/views.py
+++ supers/views.py
@@ -5,11 +5,34 @@ from .models import Super
 from supers import serializers
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
+    if request.method == 'GET':
 
-    serializer = SuperSerializer(supers, many=True)
+        hero_type = request.query_params.get('super_type')
+        print(hero_type)
 
+        supers = Super.objects.all()
+        
+        if hero_type:
+            supers = supers.filter(hero_type=hero_type)
 
-    return Response(serializer.data)
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+@api_view(['GET'])
+def super_detail(request, pk):
+    try:
+        result = Super.objects.get(pk=pk)
+        serializer = SuperSerializer(result);
+        return Response(serializer.data)
+
+    except Super.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);

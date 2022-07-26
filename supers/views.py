e6bcd2c9584ea062e8e3d067caf8f0887429d63d
unknown <colost72@gmail.com>
2022-07-25 22:16:13 -0600

    Created Get ALL Endpoint

--- supers/views.py
+++ supers/views.py
@@ -1,9 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Super
+from supers import serializers
 
 
 @api_view(['GET'])
 def supers_list(request):
+    supers = Super.objects.all()
+
+    serializer = SuperSerializer(supers, many=True)
 
 
-    return Response('ok')
+    return Response(serializer.data)
